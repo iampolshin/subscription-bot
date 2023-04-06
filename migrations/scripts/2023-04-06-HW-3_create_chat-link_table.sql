dff55cc641fc05541a822dc8886318cb6bb9791e
iampolshin <i.am.polshin@gmail.com>
2023-04-06 21:45:08 +0300

    Исправил форматирование в ченджсете

--- migrations/scripts/2023-04-06-HW-3_create_chat-link_table.sql
+++ migrations/scripts/2023-04-06-HW-3_create_chat-link_table.sql
@@ -5,6 +5,6 @@ CREATE TABLE chat_link
     chat_id bigint,
     link_id bigint,
     primary key (chat_id, link_id),
-    constraint fk_chat foreign key (chat_id) references chat(id),
-    constraint fk_link foreign key (link_id) references link(id)
+    constraint fk_chat foreign key (chat_id) references chat (id),
+    constraint fk_link foreign key (link_id) references link (id)
 );
